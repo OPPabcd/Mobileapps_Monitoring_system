1e23303e9e2c7e1d7693c9b5cf2ab2727dccd026
AHMADBAIDHOWIALIMUDIN <158020316+AHMADBAIDHOWIALIMUDIN@users.noreply.github.com>
2024-11-12 16:51:42 +0700

    Add files via upload

--- loginsetup/lib/Page/Awal.dart
+++ loginsetup/lib/Page/Awal.dart
@@ -18,38 +18,43 @@ class Awal extends StatelessWidget {
             ),
             const SizedBox(height: 5),
             // const Text(
-            //   'Green House',
+            //   'Welcome To GreenHouse',
             //   style: TextStyle(
-            //     fontSize: 50,
-            //     color: Colors.green,
+            //     fontSize: 20,
+            //     color: Color.fromARGB(255, 64, 72, 64),
             //   ),
             // ),
-            // const SizedBox(height: 5),
+            // const SizedBox(height: 3),
             // const Text(
             //   'TKK 23',
             //   style: TextStyle(
-            //     fontSize: 30,
-            //     color: Colors.green,
+            //     fontSize: 20,
+            //     color: Color.fromARGB(255, 64, 72, 64),
             //   ),
             // ),
-            // const SizedBox(height: 10),
+            const SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).pushNamed('/auth_page');
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
+                backgroundColor: const Color.fromARGB(255, 49, 186, 54),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
               ),
               child: const Text(
                 'Get In',
                 style:
                     TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
+            ),
+            Image.asset(
+              'lib/assets/background.png', // Path gambar
+              width: 300, // Lebar gambar
+              height: 300, // Tinggi gambar
             ),
           ],
         ),
