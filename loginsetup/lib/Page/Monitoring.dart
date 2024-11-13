eb331f6c9abb833f4df1c7cfc6db8d3bfcd44404
AHMADBAIDHOWIALIMUDIN <158020316+AHMADBAIDHOWIALIMUDIN@users.noreply.github.com>
2024-11-13 08:30:41 +0700

    Add files via upload

--- loginsetup/lib/Page/Monitoring.dart
+++ loginsetup/lib/Page/Monitoring.dart
@@ -1,14 +1,169 @@
 import 'package:flutter/material.dart';
 
-class Monitoring extends StatelessWidget {
-  const Monitoring({super.key});
+void main() {
+  runApp(Monitoring());
+}
 
+class Monitoring extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GreenHouseScreen(),
+    );
+  }
+}
+
+class GreenHouseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Green House"),
-        //leading : Container (),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          'assets/greenhouse.jpg'), // Sesuaikan dengan gambar yang ada di assets Anda
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 220),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Monitoring\nGreen House',
+                  style: TextStyle(
+                    color: const Color.fromARGB(169, 25, 141, 19),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 16),
+                      Text(
+                        'GreenHouse Details',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      SensorCard(
+                        icon: Icons.thermostat_outlined,
+                        label: 'Temperature',
+                        value: '25°c',
+                        color: Colors.orange,
+                      ),
+                      SensorCard(
+                        icon: Icons.water_drop,
+                        label: 'Humidity',
+                        value: '50%',
+                        color: Colors.blue,
+                      ),
+                      SensorCard(
+                        icon: Icons.wb_sunny_outlined,
+                        label: 'Light',
+                        value: '60%',
+                        color: Colors.yellow,
+                      ),
+                      SensorCard(
+                        icon: Icons.grass,
+                        label: 'Soil Humidity',
+                        value: '50%',
+                        color: Colors.brown,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SensorCard extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  final Color color;
+
+  SensorCard({
+    required this.icon,
+    required this.label,
+    required this.value,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(icon, color: color, size: 30),
+              SizedBox(width: 16),
+              Text(
+                label,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 18,
+              color: color,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
